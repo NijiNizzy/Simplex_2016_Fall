81f3b016cebc702e1ceabc230560fa83c192d16c
MAIN\ndb9897 <ndb9897@rit.edu>
2017-10-03 14:20:48 -0400

    In class changes that cater to E05.

--- 13_Translation and Scale/AppClass.cpp
+++ 13_Translation and Scale/AppClass.cpp
@@ -30,16 +30,70 @@ void Application::Display(void)
 
 	matrix4 m4View = m_pCameraMngr->GetViewMatrix();
 	matrix4 m4Projection = m_pCameraMngr->GetProjectionMatrix();
-	
-	matrix4 m4Scale = glm::scale(IDENTITY_M4, vector3(2.0f,2.0f,2.0f));
-	static float value = 0.0f;
-	matrix4 m4Translate = glm::translate(IDENTITY_M4, vector3(value, 2.0f, 3.0f));
-	value += 0.01f;
+	matrix4 m4Model = IDENTITY_M4;
+	float fMax = 2.0f;
+
+	// REPLACE STOPS FOR ARRAY LATER
+
+	vector3 v3Start = vector3(0.0f, 0.0f, 0.0f);
+	vector3 v3Stop1 = vector3(5.0f, 0.0f, 0.0f);
+	vector3 v3Stop2 = vector3(0.0f, 3.0f, 0.0f);
+	//vector3 v3End = vector3(5.0f, 0.0f, 0.0f);
+
+	/* Manual Lerp
+	vector3 v3Current = (v3Start + v3End);
+	v3Current /= 100.0f;
+	static float fPercent = 0.0f;
+	v3Current *= fPercent;
+	*/
+
+	static DWORD startTime = GetTickCount();
+	DWORD CurrentTime = GetTickCount();
+	float fCurrentTime = (CurrentTime - startTime) / 1000.0f;
+
+	float fPercent = MapValue(fCurrentTime, 0.0f, fMax, 0.0f, 1.0f);
+
+	vector3 v3Current = glm::lerp(v3Start, v3Stop1, fPercent);
+	m4Model = glm::translate(IDENTITY_M4, v3Current);
+
+	if (fCurrentTime > fMax)
+	{
+		startTime = GetTickCount();
+	}
+
+	fPercent += 0.01f;
+	if (fPercent > 1.0f)
+	{
+		fPercent = 0.0f;
+	}
+
+
+
+	//matrix4 m4Scale = glm::scale(IDENTITY_M4, vector3(2.0f,2.0f,2.0f));
+	//static float value = 0.0f;
+	//matrix4 m4Translate = glm::translate(IDENTITY_M4, vector3(value, 2.0f, 3.0f));
+	//value += 0.01f;
 
 	//matrix4 m4Model = m4Translate * m4Scale;
-	matrix4 m4Model = m4Scale * m4Translate;
 
 	m_pMesh->Render(m4Projection, m4View, m4Model);
+
+	m_pMesh->Render(m4Projection, m4View, glm::translate(IDENTITY_M4, v3Start) * glm::scale(vector3(0.1f)));
+	m_pMesh->Render(m4Projection, m4View, glm::translate(IDENTITY_M4, v3Stop1) * glm::scale(vector3(0.1f)));
+	m_pMesh->Render(m4Projection, m4View, glm::translate(IDENTITY_M4, v3Stop2) * glm::scale(vector3(0.1f)));
+
+	// place pragma region debugging information
+#pragma region Debugging Information
+	//Print info on the screen
+	uint nEmptyLines = 21;
+	for (uint i = 0; i < nEmptyLines; ++i)
+		m_pMeshMngr->PrintLine("");//Add a line on top
+								 
+	//m_pMeshMngr->Print("						");
+	m_pMeshMngr->Print("Current Time: ");//Add a line on top
+	m_pMeshMngr->PrintLine(std::to_string(fCurrentTime), C_YELLOW);
+#pragma endregion
+
 	
 	// draw a skybox
 	m_pMeshMngr->AddSkyboxToRenderList();
