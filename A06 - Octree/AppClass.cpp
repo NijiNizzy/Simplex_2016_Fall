89af2b36e8c26353b5dc0836d9d2726ae3d64e30
MAIN\ndb9897 <ndb9897@rit.edu>
2017-12-06 20:46:37 -0500

    Revert "A06 (Pt. 1)"
    This reverts commit 9e90acf6efa567c968552b150acaec424c08a386.

--- A06 - Octree/AppClass.cpp
+++ A06 - Octree/AppClass.cpp
@@ -29,9 +29,7 @@ void Application::InitVariables(void)
 			m_pEntityMngr->SetModelMatrix(m4Position);
 		}
 	}
-
 	m_uOctantLevels = 1;
-	m_pRoot = new MyOctant(m_uOctantLevels, 5); 
 	m_pEntityMngr->Update();
 }
 void Application::Update(void)
@@ -57,15 +55,7 @@ void Application::Display(void)
 	ClearScreen();
 
 	//display octree
-	if (m_uOctantID == -1)
-	{
-		m_pRoot->Display();
-	}
-	else
-	{
-		m_pRoot->Display(m_uOctantID);
-	}
-	
+	//m_pRoot->Display();
 	
 	// draw a skybox
 	m_pMeshMngr->AddSkyboxToRenderList();
--- A06 - Octree/AppClass.h
+++ A06 - Octree/AppClass.h
@@ -11,7 +11,6 @@ Date: 2017/06
 #include "imgui\ImGuiObject.h"
 
 #include "MyEntityManager.h"
-#include "MyOctant.h"
 
 namespace Simplex
 {
@@ -19,7 +18,6 @@ namespace Simplex
 class Application
 {
 	MyEntityManager* m_pEntityMngr = nullptr; //Entity Manager
-	MyOctant* m_pRoot = nullptr;
 	uint m_uOctantID = -1; //Index of Octant to display
 	uint m_uObjects = 0; //Number of objects in the scene
 	uint m_uOctantLevels = 0; //Number of levels in the octree
